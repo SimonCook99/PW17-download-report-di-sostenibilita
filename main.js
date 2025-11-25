7b19301fb7eb457606d46165480ee1c3c2cc45d6
Simone Mauro <simon.mauro@virgilio.it>
2025-11-25 10:57:32 +0100

    Aggiunta commenti vari di spiegazione

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-//configurazione animazioni con lo scroll della pagina
+//lista di tutti gli elementi della pagina da animare (classe ms_animation)
 let sections = document.querySelectorAll(".ms_animation");
 
-//richiamo la funzione dedicata (scrollanimation) sia all'evento di scroll che all'avvio/refresh della pagina
+
+//la funzione sarà richiamata sia al caricamento iniziale della pagina, sia all'evento di scroll
 scrollAnimation();
 
 window.addEventListener("scroll", scrollAnimation);
+
 
 //funzione che gestisce l'animazione di scroll delle sezioni in base all'altezza della finestra del browser
 function scrollAnimation(){
 
     //altezza della finestra
     let innerHeight = window.innerHeight;
+    console.log("Top:" + sections[2].getBoundingClientRect().top);
+    console.log("innerHeight:" + innerHeight);
+    
 
     sections.forEach(section => {
 
-        //coordinata top della singola sezione
+        //coordinata top della singola sezione rispetto alla viewport (il valore cambia dinamicamente)
         let sectionHeight = section.getBoundingClientRect().top;
 
-        //se l'altezza della finestra supera la cima, allora aggiungo la classe che fa scattare l'animazione
+        //quando la coordinata top dell'elemento diventa sufficientemente visibile 
+        //rispetto all'altezza della finestra, allora aggiungo la classe che fa scattare l'animazione
         if(sectionHeight < innerHeight - 100){
             section.classList.add("ms_animating");    
         }
